b5c30bf56e5904d07ec2be0519d40a0043b5db0e
MaxEgorov01 <egorov.maksim@b-leader.ru>
2021-06-17 21:45:15 +0300

    added all

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://opencart-2-3-0-0.egorov.t-leader.ru/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://opencart-2-3-0-0.egorov.t-leader.ru/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/egorov/data/www/opencart-2-3-0-0.egorov.t-leader.ru/catalog/');
+define('DIR_SYSTEM', '/var/www/egorov/data/www/opencart-2-3-0-0.egorov.t-leader.ru/system/');
+define('DIR_IMAGE', '/var/www/egorov/data/www/opencart-2-3-0-0.egorov.t-leader.ru/image/');
+define('DIR_LANGUAGE', '/var/www/egorov/data/www/opencart-2-3-0-0.egorov.t-leader.ru/catalog/language/');
+define('DIR_TEMPLATE', '/var/www/egorov/data/www/opencart-2-3-0-0.egorov.t-leader.ru/catalog/view/theme/');
+define('DIR_CONFIG', '/var/www/egorov/data/www/opencart-2-3-0-0.egorov.t-leader.ru/system/config/');
+define('DIR_CACHE', '/var/www/egorov/data/www/opencart-2-3-0-0.egorov.t-leader.ru/system/storage/cache/');
+define('DIR_DOWNLOAD', '/var/www/egorov/data/www/opencart-2-3-0-0.egorov.t-leader.ru/system/storage/download/');
+define('DIR_LOGS', '/var/www/egorov/data/www/opencart-2-3-0-0.egorov.t-leader.ru/system/storage/logs/');
+define('DIR_MODIFICATION', '/var/www/egorov/data/www/opencart-2-3-0-0.egorov.t-leader.ru/system/storage/modification/');
+define('DIR_UPLOAD', '/var/www/egorov/data/www/opencart-2-3-0-0.egorov.t-leader.ru/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'maksim');
+define('DB_PASSWORD', 'Q7c5X4t6');
+define('DB_DATABASE', 'opencart.egorov');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
